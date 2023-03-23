a8b70809f2a082356278206e041b548e9c37ba36
maheeb <maheeb009@gmail.com>
2023-03-24 01:33:10 +0600

    mastering has been sarted

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Slider from "@/components/home/Slider";
+import NewArrival from "@/components/home/NewArrival";
+import PopularItems from "@/components/home/PopularItems";
+import ShopMethod from "@/components/home/ShopMethod";
+
+
+export default function Home() {
+    return (
+        <>
+            <Slider/>
+            <NewArrival/>
+            <PopularItems/>
+            <ShopMethod/>
+
+        </>
+    )
+}
